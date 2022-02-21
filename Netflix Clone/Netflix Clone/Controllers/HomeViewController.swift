941701c87bde1a0e7600626a459b690e0e5e0e65
sapere4ude <eileenyoo2@gmail.com>
2022-02-21 23:57:30 +0900

    - HomeViewController Cell 작업

--- Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -30,7 +30,7 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
     }
-o
+
     private func configureNavbaar() {
         var image = UIImage(named: "Netflix")
         image = image?.withRenderingMode(.alwaysOriginal)
